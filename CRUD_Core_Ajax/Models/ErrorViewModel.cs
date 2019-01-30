0298b506cd1e7ece677af2192b90f4d809599ddd
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-30 15:10:07 +0500

    Add project files.

---
+++ CRUD_Core_Ajax/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_Core_Ajax.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

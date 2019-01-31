1f089bb5a005c978967113a298554897ef663681
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-31 14:59:14 +0500

    ✨👌 Add: Add Controller > View to Add Complex Data (temp table)

---
+++ CRUD_Core_Ajax/Controllers/ComplexDataController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Core_Ajax.Controllers
+{
+    public class ComplexDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
